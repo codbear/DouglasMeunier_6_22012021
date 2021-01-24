186560b6008ae70f7a3b3dbf36b736ad654da28d
codbear <douglas.meunier@openclassrooms.com>
2021-01-24 19:31:54 +0100

    feat(tags): create TagsCloud component

---
+++ src/modules/tags/components/TagsCloud/TagsCloud.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
+
+import TagButton from '../TagButton';
+
+const propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+}));
+
+const TagsCloud = ({ tags }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      { tags.map((tag) => (
+        <TagButton label={tag} />
+      )) }
+    </div>
+  );
+};
+
+TagsCloud.propTypes = propTypes;
+
+export default TagsCloud;
