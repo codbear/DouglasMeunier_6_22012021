b8f406f1b5eae17519af81907d42ee9ffa7875d2
codbear <douglas.meunier@openclassrooms.com>
2021-01-28 16:27:37 +0100

    refactor(app): create a Router in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
+
 import HomeScreen from './modules/home';
+import { PhotographerScreen } from './modules/photographers';
 import defaultTheme from './theme/defaultTheme';
 
 const queryClient = new QueryClient();
@@ -11,7 +19,16 @@ const App = () => (
   <ThemeProvider theme={defaultTheme}>
     <CssBaseline />
     <QueryClientProvider client={queryClient}>
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/photographers/:photographerId">
+            <PhotographerScreen />
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </QueryClientProvider>
   </ThemeProvider>
 );
