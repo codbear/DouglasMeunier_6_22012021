b57220c62ae4cf8153a0084190c0e55d9097783a
codbear <douglas.meunier@openclassrooms.com>
2021-02-27 11:56:31 +0100

    feat(App): disable refetch on window focus & allow only one retry
    in query global options

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ import { CssBaseline } from '@material-ui/core';
 import defaultTheme from './theme/defaultTheme';
 import ReactRouter from './Modules/router';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 const App = () => (
   <ThemeProvider theme={defaultTheme}>
